285f08add855aee0c7d840b0213aa1feaaa01005
jaberwokky <andreyzheltov@yandex.ru>
2016-05-27 01:59:10 +0300

    Some problems with the logic... This method is wrong -- it's erases the parent node...

--- src/main/scala/InformaticaXMLSystemFields.scala
+++ src/main/scala/InformaticaXMLSystemFields.scala
@@ -26,24 +26,21 @@ object InformaticaXMLSystemFields {
 
     object MainConnectionTransform extends RewriteRule {
       override def transform(n: Node): Seq[Node] = n match {
-        case <MAPPING>{therms @ _*}</MAPPING> => {
-            val ccd = "\"" + args(1) + "\""
-            therms.take(therms.size-1) ++ 
-                   <MAPPINGVARIABLE DATATYPE ="string" DEFAULTVALUE ={ ccd } DESCRIPTION ="" ISEXPRESSIONVARIABLE ="NO" ISPARAM ="YES" NAME ="$$mpSourceSystemCCD" PRECISION ="3" SCALE ="0" USERDEFINED ="YES"/> ++ 
-                   <MAPPINGVARIABLE DATATYPE ="integer" DEFAULTVALUE ="" DESCRIPTION ="" ISEXPRESSIONVARIABLE ="NO" ISPARAM ="YES" NAME ="$$spDwhJobID" PRECISION ="10" SCALE ="0" USERDEFINED ="YES"/> ++
-                   <MAPPINGVARIABLE DATATYPE ="date/time" DEFAULTVALUE ="" DESCRIPTION ="" ISEXPRESSIONVARIABLE ="NO" ISPARAM ="YES" NAME ="$$mpEffectiveFrom" PRECISION ="29" SCALE ="9" USERDEFINED ="YES"/> ++
-                   <MAPPINGVARIABLE DATATYPE ="date/time" DEFAULTVALUE ="" DESCRIPTION ="" ISEXPRESSIONVARIABLE ="NO" ISPARAM ="YES" NAME ="$$mpEffectiveTo" PRECISION ="29" SCALE ="9" USERDEFINED ="YES"/> ++
-                   therms(therms.size-1)
+        case map @ <MAPPING>{ therms @ _* }</MAPPING> => {
+          val ccd = "\"" + args(1) + "\""
+          therms.take(therms.size - 1) ++
+            <MAPPINGVARIABLE DATATYPE="string" DEFAULTVALUE={ ccd } DESCRIPTION="" ISEXPRESSIONVARIABLE="NO" ISPARAM="YES" NAME="$$mpSourceSystemCCD" PRECISION="3" SCALE="0" USERDEFINED="YES"/> ++
+            <MAPPINGVARIABLE DATATYPE="integer" DEFAULTVALUE="" DESCRIPTION="" ISEXPRESSIONVARIABLE="NO" ISPARAM="YES" NAME="$$spDwhJobID" PRECISION="10" SCALE="0" USERDEFINED="YES"/> ++
+            <MAPPINGVARIABLE DATATYPE="date/time" DEFAULTVALUE="" DESCRIPTION="" ISEXPRESSIONVARIABLE="NO" ISPARAM="YES" NAME="$$mpEffectiveFrom" PRECISION="29" SCALE="9" USERDEFINED="YES"/> ++
+            <MAPPINGVARIABLE DATATYPE="date/time" DEFAULTVALUE="" DESCRIPTION="" ISEXPRESSIONVARIABLE="NO" ISPARAM="YES" NAME="$$mpEffectiveTo" PRECISION="29" SCALE="9" USERDEFINED="YES"/> ++
+            therms(therms.size - 1)
         }
-        case sessfl @ <TRANSFORMATION>{trans @ _*}</TRANSFORMATION> if {
-            val attrs = sessfl.attributes.asAttrMap
-            attrs("NAME").toLowerCase.contains("fields")
-          } => {
-            trans.filter(_.label == "TRANSFORMFIELD") ++
-              <TRANSFORMFIELD DATATYPE ="string" DEFAULTVALUE ="" DESCRIPTION ="" EXPRESSION ="$$mpSourceSystemCCD" EXPRESSIONTYPE ="GENERAL" NAME ="SOURCE_SYSTEM_CCD" PICTURETEXT ="" PORTTYPE ="OUTPUT" PRECISION ="3" SCALE ="0"/> ++
-            <TRANSFORMFIELD DATATYPE ="date/time" DEFAULTVALUE ="" DESCRIPTION ="" EXPRESSION ="SYSTIMESTAMP()" EXPRESSIONTYPE ="GENERAL" NAME ="PROCESSED_DTTM" PICTURETEXT ="" PORTTYPE ="OUTPUT" PRECISION ="29" SCALE ="9"/> ++
-            <TRANSFORMFIELD DATATYPE ="string" DEFAULTVALUE ="" DESCRIPTION ="" EXPRESSION ="&apos;N&apos;" EXPRESSIONTYPE ="GENERAL" NAME ="DELETED_FLAG" PICTURETEXT ="" PORTTYPE ="OUTPUT" PRECISION ="1" SCALE ="0"/> ++
-            <TRANSFORMFIELD DATATYPE ="double" DEFAULTVALUE ="" DESCRIPTION ="" EXPRESSION ="$$spDWHJobID" EXPRESSIONTYPE ="GENERAL" NAME ="DWH_JOB_ID" PICTURETEXT ="" PORTTYPE ="OUTPUT" PRECISION ="15" SCALE ="0"/> ++
+        case trn @ <TRANSFORMATION>{ trans @ _* }</TRANSFORMATION> => {
+          trans.filter(_.label == "TRANSFORMFIELD") ++
+            <TRANSFORMFIELD DATATYPE="string" DEFAULTVALUE="" DESCRIPTION="" EXPRESSION="$$mpSourceSystemCCD" EXPRESSIONTYPE="GENERAL" NAME="SOURCE_SYSTEM_CCD" PICTURETEXT="" PORTTYPE="OUTPUT" PRECISION="3" SCALE="0"/> ++
+            <TRANSFORMFIELD DATATYPE="date/time" DEFAULTVALUE="" DESCRIPTION="" EXPRESSION="SYSTIMESTAMP()" EXPRESSIONTYPE="GENERAL" NAME="PROCESSED_DTTM" PICTURETEXT="" PORTTYPE="OUTPUT" PRECISION="29" SCALE="9"/> ++
+            <TRANSFORMFIELD DATATYPE="string" DEFAULTVALUE="" DESCRIPTION="" EXPRESSION="&apos;N&apos;" EXPRESSIONTYPE="GENERAL" NAME="DELETED_FLAG" PICTURETEXT="" PORTTYPE="OUTPUT" PRECISION="1" SCALE="0"/> ++
+            <TRANSFORMFIELD DATATYPE="double" DEFAULTVALUE="" DESCRIPTION="" EXPRESSION="$$spDWHJobID" EXPRESSIONTYPE="GENERAL" NAME="DWH_JOB_ID" PICTURETEXT="" PORTTYPE="OUTPUT" PRECISION="15" SCALE="0"/> ++
             trans.filter(_.label != "TRANSFORMFIELD")
         }
         case other => other
@@ -54,10 +51,12 @@ object InformaticaXMLSystemFields {
 
     val newXml = RuleMainConnectionTransoform(xml)
 
-    val ddd = new scala.xml.dtd.DocType("POWERMART",
-                                        scala.xml.dtd.SystemID("powrmart.dtd"),
-                                        Nil)
-    
+    val ddd = new scala.xml.dtd.DocType(
+      "POWERMART",
+      scala.xml.dtd.SystemID("powrmart.dtd"),
+      Nil
+    )
+
     scala.xml.XML.save("testxmld.xml", newXml, "windows-1251", true, ddd)
   }
 
