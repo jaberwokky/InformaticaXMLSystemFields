c583d3bcde498214363e707a492ad0d3b1337ca8
jaberwokky <andreyzheltov@yandex.ru>
2016-05-26 21:58:31 +0300

    Main transformation has been completely rewrited!

--- src/main/scala/InformaticaXMLSystemFields.scala
+++ src/main/scala/InformaticaXMLSystemFields.scala
@@ -24,29 +24,27 @@ object InformaticaXMLSystemFields {
 
     val xml = scala.xml.XML.withSAXParser(p).load(args(0))
 
-    def addChild(n: Node, newChild: Node) = n match {
-      case Elem(prefix, label, attribs, scope, child @ _*) =>
-        Elem(prefix, label, attribs, scope, child ++ newChild: _*)
-      case _ => sys.error("Can only add children to elements!")
-    }
-
     object MainConnectionTransform extends RewriteRule {
       override def transform(n: Node): Seq[Node] = n match {
-        case sesstl @ Elem(_, "MAPPING", _, _, _*) => {
-          val ccd = "\"" + args(1) + "\""
-          addChild(addChild(addChild(addChild(sesstl, <MAPPINGVARIABLE DATATYPE ="string" DEFAULTVALUE ={ ccd } DESCRIPTION ="" ISEXPRESSIONVARIABLE ="NO" ISPARAM ="YES" NAME ="$$mpSourceSystemCCD" PRECISION ="3" SCALE ="0" USERDEFINED ="YES"/>),
-            <MAPPINGVARIABLE DATATYPE ="integer" DEFAULTVALUE ="" DESCRIPTION ="" ISEXPRESSIONVARIABLE ="NO" ISPARAM ="YES" NAME ="$$spDwhJobID" PRECISION ="10" SCALE ="0" USERDEFINED ="YES"/>),
-            <MAPPINGVARIABLE DATATYPE ="date/time" DEFAULTVALUE ="" DESCRIPTION ="" ISEXPRESSIONVARIABLE ="NO" ISPARAM ="YES" NAME ="$$mpEffectiveFrom" PRECISION ="29" SCALE ="9" USERDEFINED ="YES"/>),
-            <MAPPINGVARIABLE DATATYPE ="date/time" DEFAULTVALUE ="" DESCRIPTION ="" ISEXPRESSIONVARIABLE ="NO" ISPARAM ="YES" NAME ="$$mpEffectiveTo" PRECISION ="29" SCALE ="9" USERDEFINED ="YES"/>)
+        case <MAPPING>{therms @ _*}</MAPPING> => {
+            val ccd = "\"" + args(1) + "\""
+            therms.take(therms.size-1) ++ 
+                   <MAPPINGVARIABLE DATATYPE ="string" DEFAULTVALUE ={ ccd } DESCRIPTION ="" ISEXPRESSIONVARIABLE ="NO" ISPARAM ="YES" NAME ="$$mpSourceSystemCCD" PRECISION ="3" SCALE ="0" USERDEFINED ="YES"/> ++ 
+                   <MAPPINGVARIABLE DATATYPE ="integer" DEFAULTVALUE ="" DESCRIPTION ="" ISEXPRESSIONVARIABLE ="NO" ISPARAM ="YES" NAME ="$$spDwhJobID" PRECISION ="10" SCALE ="0" USERDEFINED ="YES"/> ++
+                   <MAPPINGVARIABLE DATATYPE ="date/time" DEFAULTVALUE ="" DESCRIPTION ="" ISEXPRESSIONVARIABLE ="NO" ISPARAM ="YES" NAME ="$$mpEffectiveFrom" PRECISION ="29" SCALE ="9" USERDEFINED ="YES"/> ++
+                   <MAPPINGVARIABLE DATATYPE ="date/time" DEFAULTVALUE ="" DESCRIPTION ="" ISEXPRESSIONVARIABLE ="NO" ISPARAM ="YES" NAME ="$$mpEffectiveTo" PRECISION ="29" SCALE ="9" USERDEFINED ="YES"/> ++
+                   therms(therms.size-1)
         }
-        case sessfl @ Elem(_, "TRANSFORMATION", _, _, _*) if {
+        case sessfl @ <TRANSFORMATION>{trans @ _*}</TRANSFORMATION> if {
             val attrs = sessfl.attributes.asAttrMap
             attrs("NAME").toLowerCase.contains("fields")
           } => {
-            addChild(addChild(addChild(addChild(sessfl, <TRANSFORMFIELD DATATYPE ="string" DEFAULTVALUE ="" DESCRIPTION ="" EXPRESSION ="$$mpSourceSystemCCD" EXPRESSIONTYPE ="GENERAL" NAME ="SOURCE_SYSTEM_CCD" PICTURETEXT ="" PORTTYPE ="OUTPUT" PRECISION ="3" SCALE ="0"/>),
-              <TRANSFORMFIELD DATATYPE ="date/time" DEFAULTVALUE ="" DESCRIPTION ="" EXPRESSION ="SYSTIMESTAMP()" EXPRESSIONTYPE ="GENERAL" NAME ="PROCESSED_DTTM" PICTURETEXT ="" PORTTYPE ="OUTPUT" PRECISION ="29" SCALE ="9"/>),
-              <TRANSFORMFIELD DATATYPE ="string" DEFAULTVALUE ="" DESCRIPTION ="" EXPRESSION ="&apos;N&apos;" EXPRESSIONTYPE ="GENERAL" NAME ="DELETED_FLAG" PICTURETEXT ="" PORTTYPE ="OUTPUT" PRECISION ="1" SCALE ="0"/>),
-              <TRANSFORMFIELD DATATYPE ="double" DEFAULTVALUE ="" DESCRIPTION ="" EXPRESSION ="$$spDWHJobID" EXPRESSIONTYPE ="GENERAL" NAME ="DWH_JOB_ID" PICTURETEXT ="" PORTTYPE ="OUTPUT" PRECISION ="15" SCALE ="0"/>)
+            trans.filter(_.label == "TRANSFORMFIELD") ++
+              <TRANSFORMFIELD DATATYPE ="string" DEFAULTVALUE ="" DESCRIPTION ="" EXPRESSION ="$$mpSourceSystemCCD" EXPRESSIONTYPE ="GENERAL" NAME ="SOURCE_SYSTEM_CCD" PICTURETEXT ="" PORTTYPE ="OUTPUT" PRECISION ="3" SCALE ="0"/> ++
+            <TRANSFORMFIELD DATATYPE ="date/time" DEFAULTVALUE ="" DESCRIPTION ="" EXPRESSION ="SYSTIMESTAMP()" EXPRESSIONTYPE ="GENERAL" NAME ="PROCESSED_DTTM" PICTURETEXT ="" PORTTYPE ="OUTPUT" PRECISION ="29" SCALE ="9"/> ++
+            <TRANSFORMFIELD DATATYPE ="string" DEFAULTVALUE ="" DESCRIPTION ="" EXPRESSION ="&apos;N&apos;" EXPRESSIONTYPE ="GENERAL" NAME ="DELETED_FLAG" PICTURETEXT ="" PORTTYPE ="OUTPUT" PRECISION ="1" SCALE ="0"/> ++
+            <TRANSFORMFIELD DATATYPE ="double" DEFAULTVALUE ="" DESCRIPTION ="" EXPRESSION ="$$spDWHJobID" EXPRESSIONTYPE ="GENERAL" NAME ="DWH_JOB_ID" PICTURETEXT ="" PORTTYPE ="OUTPUT" PRECISION ="15" SCALE ="0"/> ++
+            trans.filter(_.label != "TRANSFORMFIELD")
         }
         case other => other
       }
